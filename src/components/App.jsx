fe3bec8e5b1d62323a9a5ad68f0487b813069829
Leo007ua <leo.oliinyk@ukr.net>
2023-07-18 14:48:12 +0300

    add huk by code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,46 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Buttons from './Buttons/Buttons';
 import Notification from './Notification/Notification';
 import Statistics from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [states, setStates] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  onBtnClick = event => {
+  const onBtnClick = (event) => {
     const currentName = event.target.name;
-    this.setState(prevState => {
-      return {
+    setStates(prevState => {
+      return {...prevState,
         [currentName]: prevState[currentName] + 1,
       };
     });
   };
 
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = states.good + states.neutral + states.bad;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const positive = (this.state.good * 100) / this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const positive = (states.good * 100) / countTotalFeedback();
     return positive;
   };
 
-  render() {
-    const btnKeys = Object.keys(this.state);
-    return (
-      <>
-        <Buttons onBtnClick={this.onBtnClick} btnKeys={btnKeys} />
+  const btnKeys = Object.keys(states);
+  return (
+    <>
+      <Buttons onBtnClick={onBtnClick} btnKeys={btnKeys} />
 
-        {this.countTotalFeedback() > 0 ? (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </>
-    );
-  }
-};
+{countTotalFeedback() > 0 ? (
+  <Statistics
+    good={states.good}
+    neutral={states.neutral}
+    bad={states.bad}
+    total={countTotalFeedback}
+    positivePercentage={countPositiveFeedbackPercentage}
+  />
+) : (
+  <Notification message="There is no feedback" />
+)}
+    </>
+  );
+}
